46456cc4eaec0b36b9916042ac5752aea0073f96
SubVray <jim.alberto.21@gmail.com>
2022-07-21 22:13:44 -0600

    update

---
+++ avances de proyecto 1/Listados-Registros/js/clientesInfo.js
@@ -0,0 +1,22 @@
+let clientes = [
+    {
+      ID: "#0001",
+      Nombre: "Susana Vindas Torres",
+      Usuario: "susana24",
+      numero: "******3454",
+      email : "susana24@gmail.com",
+      numero: "7365-3454",
+    },
+    {
+      ID: "#0002",
+      Nombre: "Carmen Torres Torres",
+      Usuario: "carmen24",
+      numero: "******3454",
+      email : "carmen4@gmail.com",
+      numero: "7336-3454",
+    },
+  ];
+
+
+
+  
