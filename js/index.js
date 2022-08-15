904108dd41a0191a1fd692e01792183f37cd53a2
SubVray <jim.alberto.21@gmail.com>
2022-08-15 14:29:39 -0600

    Comparasion

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var MyComparison = new slimBeforeAfter();
+
+// use refresh method for dynamicly created elements.
+// MyComparison.refresh();
+
+//use destroy method if you want to destroy plugin.
+// MyComparison.destroy();
+
