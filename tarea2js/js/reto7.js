718059a8d7a251bac3d80e1d5da6e86d14bdbbb7
SubVray <jim.alberto.21@gmail.com>
2022-10-03 21:28:16 -0600

    tarea2js

--- tarea2js/js/reto7.js
+++ tarea2js/js/reto7.js
@@ -10,34 +10,34 @@ function mostrarGames() {
 }
 function ocultarGames() {
   // la variable x muestra la lis ta por medio del ID myList
-  let musica = document.getElementById("games").innerHTML;
-  document.getElementById("resultado-juegos").innerHTML = musica;
+  let games = document.getElementById("games").innerHTML;
+  document.getElementById("resultado-juegos").innerHTML = games;
   document.getElementById("resultado-juegos").classList.add("d-none");
   document.getElementById("btn-ocultar-games").classList.add("d-none");
   document.getElementById("btn-mostrar-games").classList.remove("d-none");
 }
 
-function mostrarMusica() {
+function mostrarStreamers() {
   // la variable x muestra la lis ta por medio del ID myList
-  let musica = document.getElementById("musica").innerHTML;
-  document.getElementById("resultado-musica").innerHTML = musica;
-  document.getElementById("resultado-musica").classList.remove("d-none");
-  document.getElementById("btn-ocultar-musica").classList.remove("d-none");
-  document.getElementById("btn-mostrar-musica").classList.add("d-none");
+  let streamers = document.getElementById("streamers").innerHTML;
+  document.getElementById("resultado-streamers").innerHTML = streamers;
+  document.getElementById("resultado-streamers").classList.remove("d-none");
+  document.getElementById("btn-ocultar-streamers").classList.remove("d-none");
+  document.getElementById("btn-mostrar-streamers").classList.add("d-none");
 }
-function ocultarMusica() {
+function ocultarStreamers() {
   // la variable x muestra la lis ta por medio del ID myList
-  let peliculas = document.getElementById("musica").innerHTML;
-  document.getElementById("resultado-musica").innerHTML = peliculas;
-  document.getElementById("resultado-musica").classList.add("d-none");
-  document.getElementById("btn-ocultar-musica").classList.add("d-none");
-  document.getElementById("btn-mostrar-musica").classList.remove("d-none");
+  let streamers = document.getElementById("streamers").innerHTML;
+  document.getElementById("resultado-streamers").innerHTML = streamers;
+  document.getElementById("resultado-streamers").classList.add("d-none");
+  document.getElementById("btn-ocultar-streamers").classList.add("d-none");
+  document.getElementById("btn-mostrar-streamers").classList.remove("d-none");
 }
 
 function mostrarPeliculas() {
   // la variable x muestra la lis ta por medio del ID myList
-  let musica = document.getElementById("peliculas").innerHTML;
-  document.getElementById("resultado-peliculas").innerHTML = musica;
+  let peliculas = document.getElementById("peliculas").innerHTML;
+  document.getElementById("resultado-peliculas").innerHTML = peliculas;
   document.getElementById("resultado-peliculas").classList.remove("d-none");
   document.getElementById("btn-ocultar-peliculas").classList.remove("d-none");
   document.getElementById("btn-mostrar-peliculas").classList.add("d-none");
