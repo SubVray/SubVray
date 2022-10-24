0c4ca861d4a83d79349b81d27d485c9b7b96a0f0
SubVray <jim.alberto.21@gmail.com>
2022-10-23 18:53:34 -0600

    tarea3.js

---
+++ tareas3js/js/reto9.js
@@ -0,0 +1,6 @@
+function changeColor(event) {
+  document.querySelectorAll("span.active").forEach(function (item) {
+    item.classList.remove("active");
+  });
+  event.target.classList.add("active");
+}
