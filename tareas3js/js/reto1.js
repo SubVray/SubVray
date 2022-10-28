ced168c28df482e2b22d025229f597dc660a9dda
SubVray <jim.alberto.21@gmail.com>
2022-10-27 18:29:40 -0600

    tarea3js

--- tareas3js/js/reto1.js
+++ tareas3js/js/reto1.js
@@ -21,6 +21,22 @@ const login = () => {
 const validar = () => {
   if (username.value == "cenfo" && password.value == 123) {
     window.location.href = "bienvenidaReto1.html";
+  } else {
+    if (username.value != "cenfo") {
+      password.style.borderBottomColor = "red";
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Compruebe si los credenciales son correctos",
+      });
+    } else if (password.value != 123) {
+      password.style.borderBottomColor = "red";
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Compruebe si los credenciales son correctos",
+      });
+    }
   }
 };
 
