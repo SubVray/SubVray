c34a6d78db7874926841898cf5f56a0e94dbd173
SubVray <jim.alberto.21@gmail.com>
2022-11-17 20:20:32 -0600

    delete

--- tareas3js/js/reto9.js
+++
@@ -1,6 +0,0 @@
-function changeColor(event) {
-  document.querySelectorAll("span.active").forEach(function (item) {
-    item.classList.remove("active");
-  });
-  event.target.classList.add("active");
-}
