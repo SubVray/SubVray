145ca9bd43d701bddd545dd499aff7be0298264c
SubVray <66286368+SubVray@users.noreply.github.com>
2022-11-22 22:46:02 -0600

    jugueteria

--- juegueteriadebigotes/productosJson/products.js
+++ juegueteriadebigotes/productosJson/products.js
@@ -5,6 +5,7 @@ const productsHome = [
     sale: 0,
     price: 20795.0,
     imgUrl: "images/tienda/pistola-1.jpg",
+    cantidad: 0,
   },
   {
     name: "Perrito de Peluche con Aerógrafo ",
@@ -13,6 +14,7 @@ const productsHome = [
     price: 24995.0,
     imgUrl: "images/tienda/perrito-1.jpg",
     imgUrl2: "images/tienda/perrito-2.jpg",
+    cantidad: 0,
   },
   {
     name: "Dinosaurio T-Rex de Peluche con Aerógrafo Style 4 Ever Con 100 Plantillas",
@@ -20,6 +22,7 @@ const productsHome = [
     sale: 0,
     price: 24995.0,
     imgUrl: "images/tienda/t-rex-1.jpg",
+    cantidad: 0,
   },
   {
     name: "Minis Princesas Disney Comics Sorpresa Serie2 ",
@@ -28,6 +31,7 @@ const productsHome = [
     price: 6395,
     imgUrl: "images/tienda/minisprincesas.jpg",
     imgUrl2: "images/tienda/minisprincesas2.jpg",
+    cantidad: 0,
   },
   {
     name: "X-Shot Lanzadardos Chaos 12 Dardos-Bola ",
@@ -35,6 +39,7 @@ const productsHome = [
     sale: 0,
     price: 20795.0,
     imgUrl: "images/tienda/pistola-1.jpg",
+    cantidad: 0,
   },
 ];
 
@@ -47,6 +52,7 @@ const tiendaBebes = [
     price: 9795.0,
     imgUrl: "images/categorias/bebes/mascotaDigital-1.jpg",
     imgUrl2: "images/categorias/bebes/mascotaDigital-2.jpg",
+    cantidad: 0,
   },
   {
     name: "Perrito de Peluche con Aerógrafo ",
@@ -55,6 +61,7 @@ const tiendaBebes = [
     price: 24995.0,
     imgUrl: "images/tienda/perrito-1.jpg",
     imgUrl2: "images/tienda/perrito-2.jpg",
+    cantidad: 0,
   },
   {
     name: "Dinosaurio T-Rex de Peluche con Aerógrafo Style 4 Ever Con 100 Plantillas",
@@ -62,6 +69,7 @@ const tiendaBebes = [
     sale: 0,
     price: 24995.0,
     imgUrl: "images/tienda/t-rex-1.jpg",
+    cantidad: 0,
   },
   {
     name: "Minis Princesas Disney Comics Sorpresa Serie2 ",
@@ -70,6 +78,7 @@ const tiendaBebes = [
     price: 6395,
     imgUrl: "images/tienda/minisprincesas.jpg",
     imgUrl2: "images/tienda/minisprincesas2.jpg",
+    cantidad: 0,
   },
   {
     name: "X-Shot Lanzadardos Chaos 12 Dardos-Bola ",
@@ -77,6 +86,7 @@ const tiendaBebes = [
     sale: 0,
     price: 20795.0,
     imgUrl: "images/tienda/pistola-1.jpg",
+    cantidad: 0,
   },
 ];
 const subBebesItems = [
