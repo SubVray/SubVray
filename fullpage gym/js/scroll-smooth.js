b6fb47a082f89f9f4d080dae4e9b9b0bc71d8d28
SubVray <66286368+SubVray@users.noreply.github.com>
2022-12-05 23:01:12 -0600

    fullpage

--- fullpage gym/js/scroll-smooth.js
+++ fullpage gym/js/scroll-smooth.js
@@ -15,3 +15,48 @@ $(document).ready(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $("button #flechabtn").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        300,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+//desaparece el boton ScrollTop al cargar la pagina web
+document.getElementById("flechabtn").style.display = "none";
+
+//esconder botón ScrollTop (parte superior)
+window.onscroll = function () {
+  scrollFunction();
+};
+
+//funcion scroll
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 280 ||
+    document.documentElement.scrollTop > 280
+  ) {
+    document.getElementById("flechabtn").style.display = "block";
+    document.getElementById("whatsApp-chat").style.bottom = "100px";
+  } else {
+    document.getElementById("flechabtn").style.display = "none";
+    document.getElementById("whatsApp-chat").style.bottom = "30px";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
